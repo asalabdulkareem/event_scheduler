9b5a58b60f86fdac6a5638b45b14e0733d8a2477
asalabdulkareem <asalabdulkareem@gmail.com>
2016-12-22 04:53:10 +0000

    Implement SelectedTime model

--- app/models/selected_time.rb
+++ app/models/selected_time.rb
@@ -1,4 +1,12 @@
 class SelectedTime < ActiveRecord::Base
   belongs_to :student
-  belongs_to :event
+  validates_presence_of :student
+  validates_presence_of :from
+  validate :start_time_aligned
+  
+  def start_time_aligned
+    if from.present? and from.to_i % 30.minutes != 0
+      errors.add(:from, "is not 30-minute aligned")
+    end
+  end
 end
--- spec/models/selected_time_spec.rb
+++ spec/models/selected_time_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe SelectedTime, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:event) {
+    e = Event.create(event_type: "lecture",
+                  lecture_title: "math",
+                  description: "Calculus Lecture",
+                  email: "professor@university.edu",
+                  num_times: 1,
+                  duration: 1.5)
+    week_beginning = Time.now.beginning_of_week
+    AvailableTime.create(event: e, from: week_beginning, to: week_beginning + 1.hour)
+    return e
+  }
+  let(:student) { Student.new name: "John Smith", event: event }
+  subject { described_class.new(student: student,
+                                from: Time.now.beginning_of_week)
+  }
+  
+  it "should be valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+  
+  it "should not be valid without student" do
+    subject.student = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "should not be valid without start time" do
+    subject.from = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "should not be valid if start time is not 30-minute aligned" do
+    # we are sure Time.now.beginning_of_week is 30-minute aligned
+    # because it's exactly this week's monday midnight (00:00)
+    # thus we are testing case 00:01
+    subject.from = Time.now.beginning_of_week + 1.second
+    expect(subject).to_not be_valid
+  end
+  
 end
