bddfd603b1145c91ab96754205064f9f8817948b
asalabdulkareem <asalabdulkareem@gmail.com>
2016-12-22 02:37:54 +0000

    Add tests for the Event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(event_type: "lecture",
+                                lecture_title: "math",
+                                description: "Calculus Lecture",
+                                email: "professor@university.edu",
+                                num_times: 1,
+                                duration: 1.5)
+  }
+  
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+  
+  it "is not valid without event type" do
+    subject.event_type = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "is not valid without lecture title" do
+    subject.lecture_title = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "is not valid without description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "is not valid without email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "is not valid without duration" do
+    subject.duration = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "should not be valid if duration is not a multiple of 30 minutes" do
+    subject.duration = 0.7
+    expect(subject).to_not be_valid
+  end
+  
+  it "should not be valid if duration is negative" do
+    subject.duration = -1
+    expect(subject).to_not be_valid
+  end
+  
+  it "should not be valid if duration is zero" do
+    subject.duration = 0
+    expect(subject).to_not be_valid
+  end
+  
+  it "should automatically generate link1" do
+    expect(subject.link1).to_not be_nil
+  end
+  
+  it "should automatically generate link2" do
+    expect(subject.link2).to_not be_nil
+  end
+  
+  # tests specific for lecture type events
+  describe "Lecture" do
+    before { subject.event_type = "lecture" }
+  
+    it "is not valid without number of times per week" do
+      subject.num_times = nil
+      expect(subject).to_not be_valid
+    end
+  end
+  
+  # tests specific for exam type events
+  describe "Exam" do
+    before { subject.event_type = "exam" }
+  
+    it "is not valid with number of times per week" do
+      expect(subject).to_not be_valid
+    end
+    
+    it "is valid without number of times per week" do
+      subject.num_times = nil
+      expect(subject).to be_valid
+    end
+  end
+    
+  
 end
