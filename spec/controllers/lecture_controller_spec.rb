4c9b356ddd149bdb969814048d30aaf21e4136e3
asalabdulkareem <asalabdulkareem@gmail.com>
2016-12-25 07:08:09 +0000

    Write tests for the Lecture controller

--- spec/controllers/lecture_controller_spec.rb
+++ spec/controllers/lecture_controller_spec.rb
@@ -1,25 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe LectureController, type: :controller do
-
   describe "GET #new" do
+    before :each do
+      get :new
+    end
+    
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
     end
-  end
-
-  describe "GET #review" do
-    it "returns http success" do
-      get :review
-      expect(response).to have_http_status(:success)
+    
+    it "renders the :new template" do
+      expect(response).to render_template('new')
+    end
+    
+    it "assigns a new @lecture" do
+      expect(assigns(:lecture)).to be_a_new(Event).with(event_type: 'lecture')
     end
   end
 
-  describe "GET #create" do
+  let(:lecture) {
+    Event.new(event_type: "lecture",
+              lecture_title: "math",
+              description: "Calculus Exam",
+              email: "professor@university.edu",
+              num_times: 1,
+              duration: 1.5)
+  }
+
+  describe "POST #review" do
+    before :each do
+      post :review, lecture: lecture.attributes.except(:event_type)
+    end
+    
     it "returns http success" do
-      get :create
       expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the :review template" do
+      expect(response).to render_template('review')
+    end
+  end
+
+  describe "POST #create" do
+    before :each do
+      post :create, lecture: lecture.attributes
+    end
+    
+    it "returns http redirect" do
+      expect(response).to have_http_status(:redirect)
+    end
+    
+    it "redirects to event#success" do
+      expect(response).to redirect_to(controller: :event, action: :success, id: assigns(:exam))
+    end
+    
+    it "creates a new exam" do
+      expect {
+        post :create, lecture: lecture.attributes
+      }.to change(Event, :count).by(1)
     end
   end
 
