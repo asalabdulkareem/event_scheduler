3e6fab9418d4cedc793ea0bc7ba4acfb8871d686
asalabdulkareem <asalabdulkareem@gmail.com>
2017-01-04 18:44:02 +0000

    Timetable styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  # return 12-hour time format (eg 12:01 P.M.) from number of seconds into the day
+  def seconds_to_12_hour_time(seconds)
+    hours = seconds / 3600
+    minutes = ((seconds % 3600) / 60)
+    suffix = 'A.M.'
+    
+    if hours >= 12
+      suffix = 'P.M.'
+      
+      if hours > 12
+        hours -= 12
+      end
+    end
+    
+    time = sprintf("%d:%02d %s", hours, minutes, suffix)
+    return time
+  end
 end
